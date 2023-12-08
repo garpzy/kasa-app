6f9a5b7d5d5d99fa7cc39877f1afa3c79cd861ff
Laurene Zyskind <laurene.zyskind@gmail.com>
2023-12-08 17:07:55 +0100

    page error OK

--- src/components/ErrorMsg.jsx
+++ src/components/ErrorMsg.jsx
@@ -1,9 +1,14 @@
 import './errorMsg.scss'
+import { Link } from 'react-router-dom'
 
 function ErrorMsg(){
     return(
         <>
-        <div className='error'>404</div>
+        <div className='error-404'>404</div>
+        <p className='oups'>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/" className='error-link'>
+            <p>Retourner sur la page d’accueil</p>
+        </Link>
         </>
     )
 }
