2ad35c195a4a8ff6252cc1c67a4a13cfa0fee75c
Laurene Zyskind <laurene.zyskind@gmail.com>
2023-12-16 11:21:16 +0100

    try rating (failed)

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,36 @@
+import {useParams, Navigate } from 'react-router-dom';
+
 import './rating.scss'
+import greyStar from "../assets/grey_star.png";
+import redStar from "../assets/red_star.png";
+import logements from "../data/data.json"
 
 function Rating({note}){
+    // Récupérer le bon id pour générer une page par logement
+    let logementId = useParams('id').id;
+    let logement = logements.find(logement => logement.id === logementId);
+
+    let rating = [logement.rating]
+    let nbGreyStar = 5-rating;
+    console.log(rating)
+    console.log(nbGreyStar)
     return(
         <>
-        la note du logement
+        
+           <img
+              key={rating.toString()}
+              className="etoile"
+              src={redStar}
+              alt="red star"
+            />
+            
+            {<img
+              key={rating.toString()}
+              className="etoile"
+              src={greyStar}
+              alt="grey star"
+            />}
+            
         </>
     )
 }
