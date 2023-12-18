ccabaa721f159aa06161ffe2b815ec42c82f6c37
Laurene Zyskind <laurene.zyskind@gmail.com>
2023-12-18 21:11:00 +0100

    fix rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,8 +1,8 @@
 import {useParams, Navigate } from 'react-router-dom';
 
 import './rating.scss'
-import greyStar from "../assets/grey_star.png";
-import redStar from "../assets/red_star.png";
+import greyStarImg from "../assets/grey_star.png";
+import redStarImg from "../assets/red_star.png";
 import logements from "../data/data.json"
 
 function Rating(){
@@ -13,23 +13,43 @@ function Rating(){
     let rating = logement.rating
     let nbGreyStar = 5-rating;
 
-    let redStars = Array.from({length: rating}, (_, index) => {
-        return (<img
-            key={index}
-            className="etoile"
-            src={redStar}
-            alt="red star"
-          />);
-    })
+    // let redStars = Array.from({length: rating}, (_, index) => {
+    //     return (<img
+    //         key={index}
+    //         className="etoile"
+    //         src={redStar}
+    //         alt="red star"
+    //       />);
+    // })
 
-    let greyStars = Array.from({length: nbGreyStar}, (_, index) => {
+    // let greyStars = Array.from({length: nbGreyStar}, (_, index) => {
+    //     return (<img
+    //         key={index}
+    //         className="etoile"
+    //         src={greyStar}
+    //         alt="red star"
+    //       />);
+    // })
+
+    let redArray = Array.from({length: rating})
+    let redStars = redArray.map((index) => {
         return (<img
-            key={index}
-            className="etoile"
-            src={greyStar}
-            alt="red star"
-          />);
-    })
+                key={index}
+                className="etoile"
+                src={redStarImg}
+                alt="grey star"
+            />)
+        })
+
+    let greyArray = Array.from({length: nbGreyStar})
+    let greyStars = greyArray.map((index) => {
+        return (<img
+                key={index}
+                className="etoile"
+                src={greyStarImg}
+                alt="grey star"
+            />)
+        })
 
     return(
         <>
